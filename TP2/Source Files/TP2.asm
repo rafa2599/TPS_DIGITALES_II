04e05e3b691bcfb709331685c33d6b88d2a5113a
Rafael Fariñas <rafael.farinas@mi.unc.edu.ar>
2026-04-05 13:37:03 -0300

    Add header comments and include directives to TP2.asm

--- TP2/Source Files/TP2.asm
+++ TP2/Source Files/TP2.asm
@@ -1,1 +1,10 @@
-
+; @file Trabajo Práctico N° 2 ~ Electronica Digitales II
+; @brief
+; @detalis Implementacion para PIC16F887 en MPLAB X com MPASM
+; @author Rafael Fariñas
+; @version 1.0
+; @fecha : 3/4/2026
+    
+; DIRECTIVAS DE INCLUSION 
+LIST P = 16F887
+#include "p16f887.inc"
