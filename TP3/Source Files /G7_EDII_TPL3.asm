ba6af9be89427efbad88670ce3b68b4d8562113d
Rafael Fariñas <rafael.farinas@mi.unc.edu.ar>
2026-06-12 14:22:36 -0300

    Add macros for port configuration and delay

--- TP3/Source Files /G7_EDII_TPL3.asm
+++ TP3/Source Files /G7_EDII_TPL3.asm
@@ -23,3 +23,42 @@ CFG_PORTd   MACRO
     MOVWF   PORTD           
     ENDM
 
+CFG_PORTa   MACRO 
+    BANKSEL TRISA
+    CLRF    TRISA           ; RA0-RA3 como salidas
+    BANKSEL ANSEL
+    CLRF    ANSEL           ; Digital
+    BANKSEL PORTA
+    MOVLW   0xFF            ; PNPs apagados (Lógica negativa)
+    MOVWF   PORTA
+    ENDM
+
+DO_DELAY    MACRO v1, v2, v3
+    MOVLW   v1
+    MOVWF   D_VAR1
+    MOVLW   v2
+    MOVWF   D_VAR2
+    MOVLW   v3
+    MOVWF   D_VAR3
+    CALL    DELAY_MASTER
+    ENDM
+
+    ORG 0X00
+    GOTO    INICIO
+
+; --- PROGRAMA PRINCIPAL ---
+INICIO
+    CFG_PORTd
+    CFG_PORTa
+    
+    MOVLW   .10
+    MOVWF   CONT_WAIT       ; Vamos a parpadear 10 veces
+
+BUCLE_BLINK
+    ; --- FASE: ENCENDIDO (Multiplexado rápido durante 300ms) ---
+    MOVLW   .11             ; 11 repeticiones * 28ms = ~308ms de luz
+    MOVWF   CONT_REFRESCO
+    
+    MOVLW   B'10111111'     ; Segmento G (Bit 6 en 0)
+    MOVWF   PORTD
+    
